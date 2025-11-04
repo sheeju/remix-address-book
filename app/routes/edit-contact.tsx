3369cda26df1de41b860bf111f6e1813ae0aafd8
Sheeju Alex <sheeju@icloud.com>
2025-11-04 09:55:26 +0530

    feat: edit contact action

--- app/routes/edit-contact.tsx
+++ app/routes/edit-contact.tsx
@@ -1,7 +1,17 @@
-import { Form } from "react-router";
+import { Form, redirect } from "react-router";
 import type { Route } from "./+types/edit-contact";
 
-import { getContact } from "../data";
+import { getContact, updateContact } from "../data";
+
+export async function action({
+  params,
+  request,
+}: Route.ActionArgs) {
+  const formData = await request.formData();
+  const updates = Object.fromEntries(formData);
+  await updateContact(params.contactId, updates);
+  return redirect(`/contacts/${params.contactId}`);
+}
 
 export async function loader({ params }: Route.LoaderArgs) {
   const contact = await getContact(params.contactId);
